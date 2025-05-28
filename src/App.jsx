1959727e8e060109c87837443a2302ea936a3433
yuricifiello <yuricifiello@gmail.com>
2025-05-28 14:12:10 +0200

    add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/layouts/Header";
 import Main from "./components/layouts/Main";
 import Footer from "./components/layouts/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
